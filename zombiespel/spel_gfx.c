aa3e7da28bdf9ab2231d7e1e19fd6caad4e44cfd
Charlenze <chilenene@gmail.com>
2015-04-24 11:23:23 +0200

    Lade till en include för mac

--- zombiespel/spel_gfx.c
+++ zombiespel/spel_gfx.c
@@ -1,9 +1,10 @@
-//#include <SDL2/SDL.h>//mac
-//#include <SDL2_image/SDL_image.h>//mac
+#include <SDL2/SDL.h>//mac
+#include <SDL2_image/SDL_image.h>//mac
+#include <SDL2_ttf/SDL_ttf.h>//mac
 
-#include <SDL.h>//windows
-#include <SDL_image.h>//windows
-#include <SDL_ttf.h> // windows
+//#include <SDL.h>//windows
+//#include <SDL_image.h>//windows
+//#include <SDL_ttf.h> // windows
 
 #include <stdbool.h>
 #include "spel_structs.h"
